aa20ac860953d40c03d949c07609cec224f6bc2f
Phylis Kemuma <phylliskemuma6@gmail.com>
2026-03-20 11:51:19 +0300

    Remove all dotenv calls - use Railway environment variables

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.MYSQLDATABASE,  // database name
+  process.env.MYSQLUSER,      // username
+  process.env.MYSQLPASSWORD,  // password
   {
-    host: process.env.DB_HOST,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: 'mysql',
     logging: false
   }

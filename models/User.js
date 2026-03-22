9fd06914f29e50189258f1fa446bc3eab8d8db4c
Phylis Kemuma <phylliskemuma6@gmail.com>
2026-03-22 15:55:22 +0300

    Fix model import order - load after database connection

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db'); // NO curly braces
+const { sequelize } = require('../config/db');
 
 const User = sequelize.define('User', {
   id: {
